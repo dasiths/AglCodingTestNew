a1ec4bcc69e714de80e3f805bc1515963022160e
Dasith Wijesiriwardena <dasiths@hotmail.com>
2017-12-22 12:54:10 +1100

    Add project files.

---
+++ AglCodingTestNew/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AglCodingTestNew.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
