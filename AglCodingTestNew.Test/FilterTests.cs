1f8dcaea71edf0f2436d0593dc1f5832c1594466
Dasith Wijesiriwardena <dasiths@hotmail.com>
2017-12-31 11:10:39 +1100

    more tests

---
+++ AglCodingTestNew.Test/FilterTests.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AglCodingTestNew.Domain;
+using AglCodingTestNew.Models;
+using AglCodingTestNew.ResultFilters.TestFilter;
+using Xunit;
+
+namespace AglCodingTestNew.Test
+{
+    public class FilterTests
+    {
+        [Fact]
+        public async Task CatFilterWorks()
+        {
+            var persons = new List<TestViewModel>()
+            {
+                new TestViewModel()
+                {
+                    Gender = Gender.Male,
+                    Pets = new Pet[]
+                    {
+                        new Pet()
+                        {
+                            Name = "Dog1",
+                            Type = PetKind.Dog
+                        },
+                        new Pet()
+                        {
+                            Name = "Cat1",
+                            Type = PetKind.Cat
+                        },
+                        new Pet()
+                        {
+                            Name = "Fish1",
+                            Type = PetKind.Fish
+                        },
+                    }
+                },
+                new TestViewModel()
+                {
+                    Gender = Gender.Male,
+                    Pets = new Pet[]
+                    {
+                        new Pet()
+                        {
+                            Name = "Dog2",
+                            Type = PetKind.Dog
+                        },
+                        new Pet()
+                        {
+                            Name = "Fish2",
+                            Type = PetKind.Fish
+                        },
+                    }
+                },
+                new TestViewModel()
+                {
+                    Gender = Gender.Female,
+                    Pets = new Pet[]
+                    {
+                        new Pet()
+                        {
+                            Name = "Dog3",
+                            Type = PetKind.Dog
+                        },
+                        new Pet()
+                        {
+                            Name = "Cat3",
+                            Type = PetKind.Cat
+                        },
+                        new Pet()
+                        {
+                            Name = "Fish3",
+                            Type = PetKind.Fish
+                        },
+                    }
+                }
+            };
+
+            var filter = new CatsFilter();
+
+            var result = filter.GetFilteredResult(persons);
+
+            Assert.Equal(2, result.Count(p => p.Pets.Length > 0));
+        }
+    }
+}
--- AglCodingTestNew.Test/PyaloadTests.cs
+++ AglCodingTestNew.Test/PyaloadTests.cs
@@ -1,9 +1,13 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 using AglCodingTestNew.Mappers.MapDomain;
 using AglCodingTestNew.Mappers.MapJson;
+using AglCodingTestNew.Queries.GetDomainModel;
 using AglCodingTestNew.Queries.GetJson;
+using AglCodingTestNew.Queries.GetJson.Dtos;
 using Xunit;
 
 namespace AglCodingTestNew.Test
@@ -19,6 +23,50 @@ namespace AglCodingTestNew.Test
             var result = await outputQuery.QueryAsync(payload);
 
             Assert.Equal(6, result.Length);
+        }
+
+        [Fact]
+        public async Task IsMappedToCorrectDomainModels()
+        {
+
+            var result = new Person[] {
+                new Person()
+                {
+                    Age = 15,
+                    Gender = "Male",
+                    Name = "Guy",
+                    Pets = new List<Pet>() {new Pet()
+                    {
+                        Name = "some_pet_name",
+                        Type = "Cat"
+                    } }
+                },
+                new Person()
+                {
+                    Age = 16,
+                    Gender = "Female",
+                    Name = "Girl",
+                    Pets = new List<Pet>() {new Pet()
+                    {
+                        Name = "some_pet_name1",
+                        Type = "Cat"
+                    } }
+                },
+                new Person()
+                {
+                    Age = 15,
+                    Gender = "Male",
+                    Name = "Guy2",
+                    Pets = null
+                }
+            };
+
+            // Map result to domian model
+            var getDomainModelsFromDtosQuery = new GetDomainModelsFromDtosQuery(new JsonDtoToDomainMapper());
+            var persons = await getDomainModelsFromDtosQuery.QueryAsync(result);
+
+            Assert.Equal(3, persons.Length);
+            Assert.Equal(1, persons.Single(p => p.Name == "Guy").Pets.Count);
         }
     }
 }
