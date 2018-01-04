b4ba745d132a09f1d28fb6bde96af762c6711f3b
Dasith Wijesiriwardena <dasiths@hotmail.com>
2018-01-04 13:25:10 +1100

    add http query tests

---
+++ AglCodingTest.Core.Tests/QueryTests.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using AglCodingTest.Core.Queries.GetHttpQuery;
+using Xunit;
+
+namespace AglCodingTest.Core.Tests
+{
+    public class QueryTests
+    {
+        [Fact]
+        public async Task HttpResourceIsRetreived()
+        {
+            var query = new GetHttpResourceQuery();
+            var result = await query.QueryAsync(@"http://agl-developer-test.azurewebsites.net/people.json");
+
+            Assert.True(result.Length > 0);
+        }
+    }
+}
